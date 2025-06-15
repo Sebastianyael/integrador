2d7774de1a70ed321ae51533676b24f6232a545d
Sebastianyael <sebastianyael963@gmail.com>
2025-06-15 00:15:49 -0600

    Se completo la vista de login y se probo la conexion a una base de datos de prueba usando MySQL

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , request
 from flask_mysqldb import MySQL 
 
 app = Flask(__name__)
 
-app.config['MySQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'prueba'
+
+conexion = MySQL(app)
 
 
 @app.route('/')
@@ -11,6 +16,27 @@ def index():
     return render_template('login.html')
 
 
+@app.route('/main-feed' , methods = ['POST'])
+def feed():
+    try:
+        matricula = request.form['matricula']
+        contraseña = request.form['contraseña']
+
+        cursor = conexion.connection.cursor()
+        query = "SELECT * FROM login WHERE matricula = %s AND contraseña = %s"
+        cursor.execute(query , (matricula , contraseña))
+        result = cursor.fetchone()
+        if result:
+            return render_template('main-feed.html')
+        else:
+            return 'Error matricula o contraseña no encontrados'
+        
+    except Exception as e:
+        return f"❌ Error de conexión: {str(e)}"
+
+
 if(__name__ == '__main__'):
     app.run(debug = True)
+
+
 
--- app/static/JS/login.js
+++ app/static/JS/login.js
@@ -1,5 +1,20 @@
+const textCard = document.getElementById('text-card')
+const text = "Hola y Bienvenido al sistema de ReNova UTVT"
+const speed = 60
+let i = 0
 
-let closeButton = document.getElementById('close-button')
+function type(){
+    if(i < text.length){
+        textCard.textContent += text.charAt(i)
+        i++
+        setTimeout(type , speed)
+    }
+}
+
+type()
+
+
+const closeButton = document.getElementById('close-button') 
 
 closeButton.addEventListener('click' , () =>{
     let modal = document.getElementById('modal')
@@ -8,3 +23,30 @@ closeButton.addEventListener('click' , () =>{
     modal.classList.add('close-modal')
 })
 
+
+const enviar = document.getElementById('enviar')
+
+enviar.addEventListener('click' , (event) =>{
+    const matricula = document.getElementById('matricula')
+    const contraseña = document.getElementById('contraseña')
+    const labelTwo = document.getElementById('label-two')
+    const labelOne = document.getElementById('label-one')
+
+    labelOne.innerText = ''
+    labelTwo.innerText = ''
+
+    if(matricula.value.trim() === ''){
+        labelOne.textContent = 'Campo Vacio'
+        matricula.style.borderColor = 'red'
+        event.preventDefault()
+    }
+        
+    if(contraseña.value.trim() === ''){
+        labelTwo.textContent = 'Campo Vacio'
+        contraseña.style.borderColor = 'red'  
+        event.preventDefault()
+    }else{
+        matricula.style.borderColor = ''
+        contraseña.style.borderColor = ''
+    }
+})
