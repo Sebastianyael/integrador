11b1964510939ee8ba11bb678da62c98d79448de
Sebastianyael <sebastianyael963@gmail.com>
2025-07-11 20:28:22 -0600

    Se cambio el gestor de base de datos de Mongo Atlas a MySql , se comenzo con la vista del profesor

---
+++ app/static/JS/teacher-feed.js
@@ -0,0 +1,85 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     let fondos = document.querySelector('.fondo1');
+
+//     let observer = new IntersectionObserver(entries => {
+//         entries.forEach(entry => {
+//             if (entry.isIntersecting) {
+//                 let fondo = entry.target;
+//                 fondo.style.backgroundImage = `url('/static/images/formulario.webp')`;
+//                 observer.unobserve(fondo); 
+//             }
+//         });
+//     });
+
+//     fondos.forEach(fondo => {
+//         observer.observe(fondo);
+//     });
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     let fondos = document.querySelector('.fondo2');
+
+//     let observer = new IntersectionObserver(entries => {
+//         entries.forEach(entry => {
+//             if (entry.isIntersecting) {
+//                 let fondo = entry.target;
+//                 fondo.style.backgroundImage = `url('/static/images/grupos.webp')`;
+//                 observer.unobserve(fondo); 
+//             }
+//         });
+//     });
+
+//     fondos.forEach(fondo => {
+//         observer.observe(fondo);
+//     });
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     let fondos = document.querySelector('.fondo3');
+
+//     let observer = new IntersectionObserver(entries => {
+//         entries.forEach(entry => {
+//             if (entry.isIntersecting) {
+//                 let fondo = entry.target;
+//                 fondo.style.backgroundImage = `url('/static/images/grafica.webp')`;
+//                 observer.unobserve(fondo); 
+//             }
+//         });
+//     });
+
+//     fondos.forEach(fondo => {
+//         observer.observe(fondo);
+//     });
+// });
